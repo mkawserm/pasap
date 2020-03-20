70019c788c972581bfffa7da1efc62911b05d240
kawser <mkawserm@gmail.com>
2020-03-20 16:07:39 +0600

    CODE DOC ADDED

--- credentials.go
+++ credentials.go
@@ -1,10 +1,12 @@
 package pasap
 
+// ByteBasedEncoderCredentials implements EncoderCredentials
 type ByteBasedEncoderCredentials struct {
 	Salt     []byte
 	Password []byte
 }
 
+// ReadSalt from the given bytes
 func (b *ByteBasedEncoderCredentials) ReadSalt() (salt []byte, err error) {
 	if len(b.Salt) == 0 {
 		return nil, ErrInvalidData
@@ -13,6 +15,7 @@ func (b *ByteBasedEncoderCredentials) ReadSalt() (salt []byte, err error) {
 	return b.Salt, nil
 }
 
+// ReadPassword from the given bytes
 func (b *ByteBasedEncoderCredentials) ReadPassword() (password []byte, err error) {
 	if len(b.Password) == 0 {
 		return nil, ErrInvalidData
@@ -21,11 +24,13 @@ func (b *ByteBasedEncoderCredentials) ReadPassword() (password []byte, err error
 	return b.Password, nil
 }
 
+// ByteBasedVerifierCredentials implements VerifierCredentials
 type ByteBasedVerifierCredentials struct {
 	Password   []byte
 	EncodedKey []byte
 }
 
+// ReadPassword from the given bytes
 func (b *ByteBasedVerifierCredentials) ReadPassword() (password []byte, err error) {
 	if len(b.Password) == 0 {
 		return nil, ErrInvalidData
@@ -34,6 +39,7 @@ func (b *ByteBasedVerifierCredentials) ReadPassword() (password []byte, err erro
 	return b.Password, nil
 }
 
+// ReadEncodedKey from the given bytes
 func (b *ByteBasedVerifierCredentials) ReadEncodedKey() (encodedKey []byte, err error) {
 	if len(b.EncodedKey) == 0 {
 		return nil, ErrInvalidData
